b6928f3a796c201ec2a72fe6bfbcfca54ef33a0d
Giovanni Yuri Mita <giovanniy9@gmail.com>
2024-06-04 16:50:17 -0300

    chore: changing release steps order

--- release.config.js
+++ release.config.js
@@ -1,48 +1,37 @@
 module.exports = {
-    branches: ["main"],
-    plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/changelog", 
-      [
-        "@semantic-release/git",
-        {
-          assets: ['CHANGELOG.md', 'SemanticReleaseTest.csproj'],
-          message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-        }
-      ],
-      "@semantic-release/github",
-      [
-        "@semantic-release/commit-analyzer",
-        {
-          releaseRules: [
-            {
-              tag: "*",
-              release: "patch"
-            },
-            {
-              subject: "*",
-              release: "patch"
-            }
-          ]
-        }
-      ],
-      [
-        "@semantic-release/exec",
-        {
-          prepareCmd: `
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd: `
           VERSION=\${nextRelease.version}
           FILE="SemanticReleaseTest.csproj"
           if [ -z "$VERSION" ]; then
             echo "No version provided!"
             exit 1
           fi
-          sed -i -e "s|<Version>.*</Version>|<Version>$VERSION</Version>|" $FILE
+          if [[ "$OSTYPE" == "darwin"* ]]; then
+            sed -i '' -e "s|<Version>.*</Version>|<Version>$VERSION</Version>|" $FILE
+          else
+            sed -i -e "s|<Version>.*</Version>|<Version>$VERSION</Version>|" $FILE
+          fi
           echo "Version updated to $VERSION"
-          cat SemanticReleaseTest.csproj
+          cat $FILE
         `
-        }
-      ],
+      }
     ],
-    repositoryUrl: "https://github.com/giovannarbr/SemanticReleaseTest.git"
-  };
+    [
+      "@semantic-release/git",
+      {
+        assets: ['CHANGELOG.md', 'SemanticReleaseTest.csproj'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ],
+    "@semantic-release/github",
+  ],
+  repositoryUrl: "https://github.com/giovannarbr/SemanticReleaseTest.git"
+};
