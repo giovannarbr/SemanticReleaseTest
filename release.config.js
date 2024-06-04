88413bcdb63888d8fd722e737f3c2a8e01daece0
Giovanni Yuri Mita <giovanniy9@gmail.com>
2024-06-04 11:18:48 -0300

    updating release config

--- release.config.js
+++ release.config.js
@@ -5,8 +5,37 @@ module.exports = {
       "@semantic-release/release-notes-generator",
       "@semantic-release/changelog",
       "@semantic-release/git",
-      "@semantic-release/github"
+      "@semantic-release/github",
+      [
+        "@semantic-release/commit-analyzer",
+        {
+          releaseRules: [
+            {
+              tag: "*",
+              release: "patch"
+            },
+            {
+              subject: "*",
+              release: "patch"
+            }
+          ]
+        }
+      ],
+      [
+        "@semantic-release/exec",
+        {
+          prepareCmd: `
+          VERSION=\${nextRelease.version}
+          FILE="SemanticReleaseTest.csproj"
+          if [ -z "$VERSION" ]; then
+            echo "No version provided!"
+            exit 1
+          fi
+          sed -i '' -e "s|<Version>.*</Version>|<Version>$VERSION</Version>|" $FILE
+          echo "Version updated to $VERSION"
+        `
+        }
+      ],
     ],
     repositoryUrl: "https://github.com/giovannarbr/SemanticReleaseTest.git"
   };
-  
---
+++ update-version.sh
@@ -0,0 +1,16 @@
+# #!/bin/bash
+
+# set -e
+
+# VERSION=$1
+# FILE="SemanticReleaseTest.csproj"
+
+# if [ -z "$VERSION" ]; then
+#   echo "No version provided!"
+#   exit 1
+# fi
+
+# # Use sed to update the version in the .csproj file
+# sed -i '' -e "s|<Version>.*</Version>|<Version>$VERSION</Version>|" $FILE
+
+# echo "Version updated to $VERSION"
